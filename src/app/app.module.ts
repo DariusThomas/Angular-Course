c36d3c59ef6d5a3ff9db74d177bad4f535c4252e
Darius Thomas <dariusd.thomas@gmail.com>
2019-08-05 04:56:57 -0400

    http services included. Backend connection to FireBase established

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms"
+import { HttpClientModule } from "@angular/common/http"
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -34,7 +35,9 @@ import { RecipeService } from './recipes/recipe.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRouting
+    HttpClientModule,//for http functionality
+    AppRouting,
+
   ],
   providers: [ShoppingListService, RecipeService],
   bootstrap: [AppComponent]
